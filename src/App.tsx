faefa6013dc035dd2785f03dbc7a2410a9d31165
lixingyun918-a11y <lixingyun918@gmail.com>
2026-05-11 18:18:28 +0800

    remove initial loading delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { FramePicker } from './components/FramePicker';
 import { HeroHeader } from './components/HeroHeader';
-import { LoadingScreen } from './components/LoadingScreen';
 import { MusicButton } from './components/MusicButton';
 import { ParticleLayer } from './components/ParticleLayer';
 import { ResultPanel } from './components/ResultPanel';
@@ -159,7 +158,6 @@ function App() {
 
   return (
     <>
-      <LoadingScreen />
       <main className="app-shell">
         <ParticleLayer />
         <MusicButton />
